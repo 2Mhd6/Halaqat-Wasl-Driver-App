5ab61e51f4173c5d525875c4614bf86711557f82
Mohammed <mhds.alshammeri@gmail.com>
2025-06-26 21:43:33 +0300

    init

---
+++ lib/theme/app_color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+
+  static const appBackgroundColor = Color.fromRGBO(249, 250, 252, 98);
+  static const logoutButtonColor = Color(0xffFF0000);
+
+  // -- Buttons Colors
+  static const primaryButtonColor = Color(0xff4A6CF7);
+  static const secondaryButtonColor = Color.fromRGBO(176, 183, 195, 23);
+  static const completedButtonColor = Color(0xff1C9E48);
+
+}
